cabc3d83065b9f0cc2fd4366107e8ccbd10df34b
SumedhaVijay04 <115691663+SumedhaVijay04@users.noreply.github.com>
2023-04-18 10:19:11 +0530

    Update Addition.js

--- Addition.js
+++ Addition.js
@@ -1,1 +1,22 @@
-
+function input()
+{
+  a=parseInt(prompt("Enter number a:"));
+  b=parseInt(prompt("Enter number b:"));
+  return[a,b];
+}
+function add(a,b)
+{
+  return a+b;
+}
+function output(a,b,c)
+{
+  console.log("%d+%d is %d",a,b,c) 
+}
+function main()
+{
+  let a,b;
+  [a,b]=input();
+  let c=add(a,b);
+  output(a,b,c);
+}
+main()
